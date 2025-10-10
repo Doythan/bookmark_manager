f8d6543e70f03e2ce529b1cc4ed2563ab276b5a0
도경원 <won03289@gmail.com>
2025-10-10 14:23:49 +0900

    feat: debugShowCheckedModeBanner false 설정

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Bookmark Manager',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
